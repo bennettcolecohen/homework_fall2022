9de20581b8d5cc22fbb8c6b0f5f6dbef7a8346d0
Bennett Cohen <bennettcohen@Bennetts-MacBook-Air.local>
2022-10-04 11:15:16 -0700

    sending back

--- hw3/cs285/scripts/create_plots.py
+++ hw3/cs285/scripts/create_plots.py
@@ -1,5 +1,6 @@
 from tensorflow.python.summary.summary_iterator import summary_iterator
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import os
 
@@ -19,18 +20,43 @@ def read_data(path):
                 best_returns.append(v.simple_value)
     return steps, avg_returns, best_returns
 
-# Question 1 Plot
-q1_path = 'q1_MsPacman'
-steps, avg_returns, best_returns = read_data(q1_path)
-steps = steps[:steps.index(1000001.0)+1]
-avg_returns = avg_returns[:len(steps)]
-best_returns = best_returns[:len(steps)]
+def plot_data(x, list_of_y, list_of_labels, title, xlabel, ylabel, legend, output_file):
 
-fig, ax = plt.subplots(figsize = (8,6))
-plt.plot(steps, avg_returns, label = 'Average Return')
-plt.plot(steps, best_returns, label = 'Best Return')
-plt.xlabel('Steps')
-plt.ylabel('Return')
-plt.title('MsPacman-v0 DQN Returns')
-plt.legend()
-fig.savefig('q1_dqn.png')
+    fig, ax = plt.subplots(figsize = (8,6))
+
+    for idx, y in enumerate(list_of_y): 
+        plt.plot(x, y, label = list_of_labels[idx])
+
+    plt.title(title), plt.xlabel(xlabel), plt.ylabel(ylabel)
+    if legend: 
+        plt.legend()
+    fig.savefig(output_file)
+
+
+
+#Q1 
+# q1_path = 'q1_MsPacman'
+# steps, avg_returns, best_returns = read_data(q1_path)
+# steps = steps[:steps.index(1000001.0)+1]
+# avg_returns = avg_returns[:len(steps)]
+# best_returns = best_returns[:len(steps)]
+# plot_data(steps, [avg_returns, best_returns], ['Avg. Return', 'Best Return'],'MsPacman-v0 DQN Returns', 'Steps', 'Returns', True, 'q1_dqn.png')
+
+#Q2
+dqn_avg_returns, ddqn_avg_returns = [],[],
+for i in [1,2,3]: 
+    dqn_steps, dqn_avg, _ = read_data(f'q2_dqn_{i}')
+    _, ddqn_avg, _ = read_data(f'q2_doubledqn_{i}')
+
+    dqn_avg_returns.append(dqn_avg)
+    ddqn_avg_returns.append(ddqn_avg)
+
+dqn_avg_avg = np.mean(np.array(dqn_avg_returns), axis = 0)
+ddqn_avg_avg = np.mean(np.array(dqn_avg_returns), axis = 0)
+
+
+plot_data(dqn_steps[1:], [dqn_avg_avg, ddqn_avg_avg], 
+['DQN Avg.', 'Double DQN Avg.'],
+'LunarLander-v3 DQN & Double DQN Returns', 'Steps', 'Returns', True, 'q2.png')
+
+
